cce45734c6775c130639c519097d13eede805995
Cameron Ketcham <cketcham@gmail.com>
2014-05-17 12:04:53 -0700

    updated error handling

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,5 +1,17 @@
 export default Ember.Route.extend(Ember.SimpleAuth.ApplicationRouteMixin, {
   actions: {
+    sessionAuthenticationFailed: function(error) {
+      this.send('showError', error);
+    },
+    showError: function(error) {
+      var controller = this.controllerFor('application');
+      controller.set('success', null);
+      controller.set('error', error || "Unknown Error");
+    },
+    showSuccess: function(success) {
+      var controller = this.controllerFor('application');
+      controller.set('success', success);
+      controller.set('error', null);
     }
   }
 });
