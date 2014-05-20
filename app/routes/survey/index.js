760f81d202aad2dce8d778c279e230e0283a28e0
Cameron Ketcham <cketcham@gmail.com>
2014-05-20 11:52:43 -0700

    refresh the model if it doesn't have survey items

--- app/routes/survey/index.js
+++ app/routes/survey/index.js
@@ -1,7 +1,18 @@
 export default Ember.Route.extend({
   model: function(params) {
-    return this.store.find('survey', params.survey_id).then(function(record) {
-      return record.reload();
-    })
+    return this.store.find('survey', params.survey_id);
+  },
+
+  /**
+    This will check to make sure the model has survey items
+    before it is shown. If it doesn't it will go to the
+    network.
+  */
+  setupController: function (controller, model) {
+    if(!model.get('survey_items')) {
+      model.reload().then(function(model) {
+        controller.set("model", model);
+      })
+    }
   }
 });
