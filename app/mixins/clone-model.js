90674a216fc107be94c07a211bd10832dd5f38fb
Cameron Ketcham <cketcham@gmail.com>
2014-06-09 11:36:03 -0400

    fixed bug where cloned model would not be redownloaded

--- app/mixins/clone-model.js
+++ app/mixins/clone-model.js
@@ -2,7 +2,20 @@
   Set a cloned version of the model on the controller
 */
 export default Ember.Mixin.create({
+
   setupController: function (controller, model) {
-    this._super(controller, model.clone());
+    var self = this;
+    if(model.get('id') && !model.isLoaded()) {
+      model.reload().then(function(model) {
+        self.controller.set("model", model.clone());
+      });
+    } else {
+      this.controller.set("model", model.clone());
+    }
   },
+
+  // For now we need to make sure the model is up to date and then clone it
+  // setupController: function (controller, model) {
+  //   this._super(controller, model.clone());
+  // },
 });
