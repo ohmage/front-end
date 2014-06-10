4eda0c2e2bab122b2d3f24bca6cab007e88f682d
Cameron Ketcham <cketcham@gmail.com>
2014-06-10 17:08:55 -0400

    fixed some css for the menu

---
+++ app/components/sidebar-wrapper.js
@@ -0,0 +1,28 @@
+export default Ember.Component.extend({
+  classNames: ['sidebar-collapse'],
+
+  classNameBindings: ['collapse'],
+
+  collapse: function() {
+    return ((window.innerWidth > 0) ? window.innerWidth : screen.width) < 768;
+  }.property('width'),
+
+  _resizeHandler: null,
+
+  didInsertElement: function() {
+    this._super();
+    // we save the binded resize function to keep the identity, so unbind will remove
+    var self = this;
+    this._resizeHandler = function() {
+      var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+      self.set('width', width);
+    };
+
+    $(window).bind('load resize', this._resizeHandler);
+  },
+
+  willClearRender: function() {
+    this._super();
+    $(window).unbind('load resize', this._resizeHandler);
+  }
+});
