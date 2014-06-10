0b47f10ea9d6347d3d855ff1fa1b87e2fe54cbc4
Cameron Ketcham <cketcham@gmail.com>
2014-06-10 18:14:21 -0400

    sidebar should open automatically to the section for the content

--- app/views/home.js
+++ app/views/home.js
@@ -2,6 +2,10 @@ export default Ember.View.extend({
   didInsertElement: function() {
     Ember.run.scheduleOnce('afterRender', this, function() {
       $('#side-menu').metisMenu();
+
+      // Open any menus which have active links
+      $('li ul').has('a.active').addClass('collapse in');
+      $('li ul').not(':has(a.active)').addClass('collapse');
     });
   }
 });
